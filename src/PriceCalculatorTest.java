84d6795742cdcbb6a9ad081f3e54390616d56105
tahirdotz <tahirzaman075@gmail.com>
2024-11-20 16:30:33 +0600

    Test didnt pass

---
+++ src/PriceCalculatorTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PriceCalculatorTest {
+
+    @Test
+    public void testCalculateSubtotalWithContainerAndIceCreamItems() {
+        Container container = new Container("Waffle Cone", 5.00);
+        PriceCalculator calculator = new PriceCalculator(container);
+        Flavor flavor1=new Flavor("Mint Chocolate Chip",2.80);
+        OrderItem item1 = new OrderItem(flavor1, 2);
+
+
+        calculator.addIceCreamItem(item1);
+
+        double expectedSubtotal = 5.00 + 8.60;
+
+        assertEquals(expectedSubtotal, calculator.calculateSubtotal(), "Subtotal should be correctly calculated.");
+    }
+
+
+
+
+}
